cb2cfb1e8238f630fe37cbe40b85795fcacc98aa
Thiago Henrique dos Santos <henrique.thsantos.ths@gmail.com>
2018-08-24 02:16:39 -0300

    A small example of interest calculation
    In this example a simulation of compound interest over time

---
+++ src/com/codedemonbr/Interest.java
@@ -0,0 +1,24 @@
+package com.codedemonbr;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+
+public class Interest {
+
+    public static void main(String[] args) {
+	// write your code here
+        BigDecimal principal = BigDecimal.valueOf(1000.0);
+        BigDecimal rate = BigDecimal.valueOf(0.05);
+
+        System.out.printf("%s%20s%n", "Year", "Amount on deposit");
+
+
+        for (int year = 1; year < 11 ; year++)
+        {
+            BigDecimal amount = principal.multiply(rate.add(BigDecimal.ONE).pow(year));
+
+            System.out.printf("%4d%20s%n", year, NumberFormat.getCurrencyInstance().format(amount));
+            
+        }
+    }
+}
